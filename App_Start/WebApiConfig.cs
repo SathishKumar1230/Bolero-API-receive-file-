513c1c7c8760d86ec72b8e3fe9ac5e87897a6021
SathishKumar1230 <98320040+SathishKumar1230@users.noreply.github.com>
2023-03-21 01:49:07 +0530

    App_start commit

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Web.Http.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Routing;
+
+
+namespace Msc.Agency.DigitalBL
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.AddApiVersioning();
+            var constraintResolver = new DefaultInlineConstraintResolver()
+            {
+                ConstraintMap = { ["apiVersion"] = typeof(ApiVersionRouteConstraint) }
+            };
+
+            // Web API routes
+            config.MapHttpAttributeRoutes(constraintResolver);
+
+        }
+    }
+}
