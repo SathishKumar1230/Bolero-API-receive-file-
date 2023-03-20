dfe31ab2a0a1318da6dc2bd9d23372b66267f054
SathishKumar1230 <98320040+SathishKumar1230@users.noreply.github.com>
2023-03-21 01:52:03 +0530

    bussiness layer commit

---
+++ Business/BaseBusinesscs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Msc.Agency.DigitalBL.Business
+{
+    public class BaseBusiness<T> where T : class
+    {
+        private static readonly Lazy<T> DataInstance = new Lazy<T>(GetData, System.Threading.LazyThreadSafetyMode.ExecutionAndPublication);
+
+        /// <summary>
+        /// Gets the data reader.
+        /// </summary>
+        /// <value>
+        /// The data reader.
+        /// </value>
+        protected static T Resolve
+        {
+            get
+            {
+                return DataInstance.Value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the data reader.
+        /// </summary>
+        /// <returns>Generic Type.</returns>
+        private static T GetData()
+        {
+            return Activator.CreateInstance<T>();
+        }
+    }
+}
---
+++ Business/DigitalBLBusiness.cs
@@ -0,0 +1,56 @@
+﻿using Msc.Agency.DigitalBL.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Web;
+
+namespace Msc.Agency.DigitalBL.Business
+{
+    public class DigitalBLBusiness : BaseBusiness<DataAction>
+    {
+        public async static Task<bool> SaveZipFileResponse(Msc.Agency.DigitalBL.Models.Rootobject rootobject)
+        {
+            using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                var dbSave = await Resolve.SaveZipFileResponse(rootobject);
+                scope.Complete();
+            }
+            return true;
+        }
+
+        public async static Task<bool> SaveXmlFileResponse(Rootobject rootobject)
+        {
+            using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                var dbSave = await Resolve.SaveXmlFileResponse(rootobject);
+               // DigitalBLBusiness.LogUpdation(res);
+
+                scope.Complete();
+            }
+            return true;
+        }
+
+        //public async static Task<bool> LogUpdation(string response, string transactionId)
+        //{
+        //    using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+        //    {
+        //        var dbSave = await Resolve.LogUpdation(response, transactionId);
+        //        scope.Complete();
+        //    }
+        //    return true;
+        //}
+
+        public async static Task<bool> LogUpdation(string response, string transactionId, string fileType,string ConnectionString,string pdffile)
+        {
+            using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                var dbSave = await Resolve.LogUpdation(response, transactionId, fileType,ConnectionString,pdffile);
+                scope.Complete();
+            }
+            return true;
+        }
+
+    }
+}
